f9e831d5f898be1a009907f09e64de934572c687
GULBENTOKALAK <GULBENTOKALAK@TURALGULIYEV>
2020-07-15 17:06:01 +0300

    log4net ekleme

--- ECommerceApi/Startup.cs
+++ ECommerceApi/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace ECommerceApi
 {
@@ -28,15 +29,15 @@ namespace ECommerceApi
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-            
-            app.ConfigureCustomExceptionMiddleware();
 
+            app.ConfigureCustomExceptionMiddleware();
+            loggerFactory.AddLog4Net();
             app.UseHttpsRedirection();
 
             app.UseRouting();
